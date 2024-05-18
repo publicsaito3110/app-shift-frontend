bf8dfcc5ca710055e4fd68231fad37370c556ef4
publicsaito3110 <public.business.saito3110@gmail.com>
2024-05-18 15:12:59 +0900

    add: logout processing

---
+++ src/logout/Logout.vue
@@ -0,0 +1,29 @@
+<template>
+</template>
+
+<script>
+import axios from 'axios';
+import constant from '@/common/constant'
+
+export default {
+  created() {
+    this.$store.dispatch('removeToken')
+    this.$router.push({ path: '/login' })
+  },
+  methods: {
+    apiUser() {
+      axios.post(constant.BE_API_URL + '/api/user', {
+        id: this.id
+      })
+        .then(res => {
+          console.log(res.data)
+          this.$router.push({ 
+            path: '/user',
+            params: { res: res.data.userList[0].id }
+          });
+        }).catch(err => { throw err })
+    }
+
+  }
+}
+</script>
